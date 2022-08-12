7412aaa150e6fd44d45cec1a57e4801849516cdb
sebamstrr <mccrash2018@example.com>
2022-08-12 13:01:25 -0400

    sirve

---
+++ tapia_sebastian_JSPredicción.js
@@ -0,0 +1,28 @@
+function myBirthYearFunc(){
+        console.log("Nací en " + 1980);
+    }
+    myBirthYearFunc(año)
+    var año = 2005
+    /*console.log naci en 2005*/
+    function myBirthYearFunc(año){
+            console.log("Nací en " + año);
+        }
+        function add(num1, num2){
+                console.log("¡Sumando números!");
+                console.log("num1 is: " + num1);
+                console.log("num2 is: " + num2);
+                var sum = num1 + num2;
+                console.log(sum);
+            }
+    var num1 = 10
+    var num2 = 20
+    function add(num1, num2){
+            console.log("¡Sumando números!");
+            console.log("num1 is: " + num1);
+            console.log("num2 is: " + num2);
+            var sum = num1 + num2;
+            console.log(sum);
+        }
+        //console.log inprime el resultado de la suma de num1 y num2//
+        
+        
