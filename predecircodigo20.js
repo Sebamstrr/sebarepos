d5e1d1d416db2e0a767d8f405da51ed1a0c8896d
sebamstrr <mccrash2018@example.com>
2022-08-24 12:34:54 -0400

    listo:)

--- predecircodigo20.js
+++ predecircodigo20.js
@@ -63,6 +63,58 @@ console.log('result is', result);
 
 //console.log inprime | ('result is (3*2) + (5*2) = 16)
 
+function sumNums(num1, num2) {  
+    return num1+num2;
+ }
+ console.log(sumNums(2,3))
+ console.log(sumNums(3,5))
+
+ //console.log inprime | sumNums(num1, num2) = 5,8
+
+ function printSumNums(num1, num2) {
+    console.log(num1);   
+    return num1+num2;
+ }
+ console.log(printSumNums(2,3))
+ console.log(printSumNums(3,5))
+ 
+ //console.log inprime | 2 luego suma ambos numeros, resultado es 5.
+ //console.log imprime | 3 luego suma ambos numero, resultado es 8
+
+ function sumNums(num1, num2) {
+    var sum = num1 + num2;
+    console.log('sum is', sum);
+    return sum;
+}
+var result = sumNums(2,3) + sumNums(3,5);
+console.log('result is', result);
+
+/*
+    console.log('sum is', sum);  --> sum is 5
+    console.log('sum is', sum);  --> sum is 8
+console.log('result is', result); | result is 13
 
 
+*/
+function sumNums(num1, num2) {
+    var sum = num1 + num2;
+    console.log('sum is', sum);
+    return sum;
+}
 
+var result = sumNums(2,3)//5
+
+
+var result = sumNums(2,3) + sumNums(3,
+    sumNums(2,1))
+     + 
+     sumNums(sumNums(2,1),sumNums(2,3));
+console.log('result is', result);
+
+/*
+console.log imprime --> 19
+Diagrama en T
+variable valor
+num1     2,3,2,2,2
+num2     3,1,1,3
+*/
