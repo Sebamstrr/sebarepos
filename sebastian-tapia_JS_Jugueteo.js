81997fb29e7634f63d8ac617baf5ae2a2325eabd
sebamstrr <mccrash2018@example.com>
2022-08-26 11:14:41 -0400

    h

---
+++ sebastian-tapia_JS_Jugueteo.js
@@ -0,0 +1,22 @@
+var alturaNiño = 55
+ function muestraSiElNiñoPuedeSubirALaMontañaRusa(alturaNiño){
+    if (alturaNiño > 52)
+    {
+        console.log("¡pasa, puedes subir!")
+    } else {
+        console.log("Lo siento hrmano Tal vez el próximo año")
+    }
+}
+
+
+/*
+var edad = 50;
+function mayordeEdad(){
+    if (edad > 18){
+        console.log("Es mayor edad")
+       
+    } else {
+        console.log("Es menor de edad");
+    }
+}
+*/
